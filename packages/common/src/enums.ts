f314eaec4118bc185cbf927009f06adc0963ecac
Akash Shaw <shawakash2003@gmail.com>
2024-04-15 07:34:51 +0530

    feat: adding a common package

---
+++ packages/common/src/enums.ts
@@ -0,0 +1,9 @@
+export enum responseStatus {
+    Ok = "ok",
+    Error = "error",
+}
+
+export enum dbResStatus {
+    Ok = "ok",
+    Error = "error",
+}
---
+++ packages/common/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./enums";
+export * from "./validations";
+export * from "./constants";
