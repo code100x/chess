877f3c99ce8b7a9e5a12898bdca4e45ebd4e7993
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-04-19 01:28:14 +0530

    Changed the Deafult value of gameover

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -11,7 +11,7 @@ export const GAME_OVER = "game_over";
 
 export const Game = () => {
     const socket = useSocket();
-    const [gameOver, setGameOver] = useState(true);
+    const [gameOver, setGameOver] = useState(false);
     const [chess, setChess] = useState(new Chess());
     const [board, setBoard] = useState(chess.board());
     const [started, setStarted] = useState(false)
