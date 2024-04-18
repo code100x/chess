e11ad593b0240850e48a12a0fd5f36b009fd3be4
vishrudh raj <vishrudh.shrinivas@gmail.com>
2024-04-18 23:21:00 +0530

    made ui similar to chess.com

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { Game } from './screens/Game';
 
 function App() {
   return (
-    <div className='h-screen bg-slate-950'>
+    <div className='h-screen bg-[#302E2B]'>
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} /> 
