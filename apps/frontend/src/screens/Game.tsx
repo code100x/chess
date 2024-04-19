a5fbabd8dc475e8acf453787c881e21dee21c107
aakash2330 <aakash2330@gmail.com>
2024-04-19 22:52:52 +0530

    invalid-self-game

--- apps/frontend/src/screens/Game.tsx
+++ apps/frontend/src/screens/Game.tsx
@@ -16,93 +16,93 @@ export const OPPONENT_DISCONNECTED = "opponent_disconnected";
 export const GAME_OVER = "game_over";
 
 export interface IMove {
-    from: Square; to: Square
+  from: Square; to: Square
 }
 
 interface Metadata {
-    blackPlayer: { id: string, name: string };
-    whitePlayer: {id: string, name: string };
+  blackPlayer: { id: string, name: string };
+  whitePlayer: { id: string, name: string };
 }
 
 export const Game = () => {
-    const socket = useSocket();
-    const { gameId } = useParams();
-    const user = useUser();
+  const socket = useSocket();
+  const { gameId } = useParams();
+  const user = useUser();
 
-    const navigate = useNavigate();
-    // Todo move to store/context
-    const [chess, _setChess] = useState(new Chess());
-    const [board, setBoard] = useState(chess.board());
-    const [started, setStarted] = useState(false)
-    const [gameMetadata, setGameMetadata] = useState<Metadata | null>(null)
-    const [result, setResult] = useState<"WHITE_WINS" | "BLACK_WINS" | "DRAW" | typeof OPPONENT_DISCONNECTED | null>(null);
-    const [moves, setMoves] = useState<IMove[]>([]);
+  const navigate = useNavigate();
+  // Todo move to store/context
+  const [chess, _setChess] = useState(new Chess());
+  const [board, setBoard] = useState(chess.board());
+  const [started, setStarted] = useState(false)
+  const [gameMetadata, setGameMetadata] = useState<Metadata | null>(null)
+  const [result, setResult] = useState<"WHITE_WINS" | "BLACK_WINS" | "DRAW" | typeof OPPONENT_DISCONNECTED | null>(null);
+  const [moves, setMoves] = useState<IMove[]>([]);
 
-    useEffect(() => {
-        if (!socket) {
-            return;
-        }
-        socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      switch (message.type) {
+        case INIT_GAME:
+          if (message.payload.blackPlayer.id === message.payload.whitePlayer.id) return alert("can't start game with same user")
+          setBoard(chess.board());
+          setStarted(true)
+          navigate(`/game/${message.payload.gameId}`)
+          setGameMetadata({
+            blackPlayer: message.payload.blackPlayer,
+            whitePlayer: message.payload.whitePlayer
+          })
+          break;
+        case MOVE:
+          const move = message.payload;
+          chess.move(move);
+          setBoard(chess.board());
+          setMoves(moves => [...moves, move])
+          break;
+        case GAME_OVER:
+          setResult(message.payload.result);
+          break;
 
-            switch (message.type) {
-                case INIT_GAME:
-                    setBoard(chess.board());
-                    setStarted(true)
-                    navigate(`/game/${message.payload.gameId}`)
-                    setGameMetadata({
-                        blackPlayer: message.payload.blackPlayer,
-                        whitePlayer: message.payload.whitePlayer
-                    })
-                    break;
-                case MOVE:
-                    const move = message.payload;
-                    chess.move(move);
-                    setBoard(chess.board());
-                    setMoves(moves => [...moves, move])
-                    break;
-                case GAME_OVER:
-                    setResult(message.payload.result);
-                    break;
+        case OPPONENT_DISCONNECTED:
+          setResult(OPPONENT_DISCONNECTED)
+          break;
+      }
+    }
+  }, [chess, socket]);
 
-                case OPPONENT_DISCONNECTED:
-                    setResult(OPPONENT_DISCONNECTED)
-                    break;
-            }
-        }
-    }, [chess, socket]);
+  if (!socket) return <div>Connecting...</div>
 
-    if (!socket) return <div>Connecting...</div>
-
-    return <div className="">
-        <div className="justify-center flex pt-4 text-white">
-            {gameMetadata?.blackPlayer?.name} vs {gameMetadata?.whitePlayer?.name}
-        </div>
-        {result && <div className="justify-center flex pt-4 text-white">
-            {result}    
-        </div>}
-        <div className="justify-center flex">
-            <div className="pt-8 max-w-screen-lg w-full">
-                <div className="grid grid-cols-6 gap-4 w-full">
-                    <div className="col-span-4 w-full flex justify-center">
-                        <ChessBoard myColor={user.id === gameMetadata?.blackPlayer?.id ? "b" : "w"} setMoves={setMoves} moves={moves} chess={chess} setBoard={setBoard} socket={socket} board={board} />
-                    </div>
-                    <div className="col-span-2 bg-slate-900 w-full flex justify-center">
-                        <div className="pt-8">
-                            {!started && gameId === "random" && <Button onClick={() => {
-                                socket.send(JSON.stringify({
-                                    type: INIT_GAME
-                                }))
-                            }} >
-                                Play
-                            </Button>}
-                        </div>
-                        <div className="mr-10">            
-                            {moves.length > 0 && <div className="mt-4"><MovesTable moves={moves} /></div>}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  return <div className="">
+    <div className="justify-center flex pt-4 text-white">
+      {gameMetadata?.blackPlayer?.name} vs {gameMetadata?.whitePlayer?.name}
     </div>
+    {result && <div className="justify-center flex pt-4 text-white">
+      {result}
+    </div>}
+    <div className="justify-center flex">
+      <div className="pt-8 max-w-screen-lg w-full">
+        <div className="grid grid-cols-6 gap-4 w-full">
+          <div className="col-span-4 w-full flex justify-center">
+            <ChessBoard myColor={user.id === gameMetadata?.blackPlayer?.id ? "b" : "w"} setMoves={setMoves} moves={moves} chess={chess} setBoard={setBoard} socket={socket} board={board} />
+          </div>
+          <div className="col-span-2 bg-slate-900 w-full flex justify-center">
+            <div className="pt-8">
+              {!started && gameId === "random" && <Button onClick={() => {
+                socket.send(JSON.stringify({
+                  type: INIT_GAME
+                }))
+              }} >
+                Play
+              </Button>}
+            </div>
+            <div className="mr-10">
+              {moves.length > 0 && <div className="mt-4"><MovesTable moves={moves} /></div>}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 }
