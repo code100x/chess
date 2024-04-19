395267055b31f633e5e0b9f24bd9938915951fdd
harkirat <harkirat96@gmail.com>
2024-04-19 20:31:48 +0530

    Added moves table

--- apps/frontend/src/components/ChessBoard.tsx
+++ apps/frontend/src/components/ChessBoard.tsx
@@ -1,11 +1,13 @@
-import { Chess, Color, PieceSymbol, Square } from "chess.js";
+import { Chess, Color, Move, PieceSymbol, Square } from "chess.js";
 import { useState } from "react";
-import { MOVE } from "../screens/Game";
+import { IMove, MOVE, } from "../screens/Game";
 import MoveTable from "./MovesTable";
 
 
-export const ChessBoard = ({ chess, board, socket, setBoard }: {
+export const ChessBoard = ({ chess, board, socket, setBoard, moves, setMoves }: {
     chess: Chess;
+    moves: IMove[];
+    setMoves: React.Dispatch<React.SetStateAction<IMove[]>>;
     setBoard: React.Dispatch<React.SetStateAction<({
         square: Square;
         type: PieceSymbol;
@@ -19,14 +21,10 @@ export const ChessBoard = ({ chess, board, socket, setBoard }: {
     socket: WebSocket;
 }) => {
     const [from, setFrom] = useState<null | Square>(null);
-    const [moves, setMoves] = useState<{ from: Square; to: Square }[]>([]);
 
     return (
         <div className="flex">
-        <div className="mr-10">            
-            {moves.length > 0 && <div className="mt-4"><MoveTable moves={moves} /></div>}
-        </div>
-         <div className="text-white-200 mr-10">
+        <div className="text-white-200 mr-10">
         {board.map((row, i) => {
             return <div key={i} className="flex">
                 {row.map((square, j) => {
@@ -45,7 +43,6 @@ export const ChessBoard = ({ chess, board, socket, setBoard }: {
                                     }
                                 }
                             }))
-                            
                             setFrom(null)
                             chess.move({
                                 from,
@@ -56,12 +53,13 @@ export const ChessBoard = ({ chess, board, socket, setBoard }: {
                                 from,
                                 to: squareRepresentation
                             })
-                            setMoves([...moves, { from, to: squareRepresentation }]);
+                            setMoves(moves =>[...moves, { from, to: squareRepresentation }]);
                         }
                     }} key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-green-500' : 'bg-slate-500'}`}>
                         <div className="w-full justify-center flex h-full">
                             <div className="h-full justify-center flex flex-col">
                                 {square ? <img className="w-4" src={`/${square?.color === "b" ? square?.type : `${square?.type?.toUpperCase()} copy`}.png`} /> : null} 
+
                             </div>
                         </div>
                     </div>
