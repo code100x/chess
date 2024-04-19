0f3b0e4d2a8e372bfb501cffb8eb1a9537b84f1b
harkirat <harkirat96@gmail.com>
2024-04-19 19:50:24 +0530

    Added names on the frontend

--- apps/frontend/src/screens/Game.tsx
+++ apps/frontend/src/screens/Game.tsx
@@ -12,6 +12,11 @@ export const INIT_GAME = "init_game";
 export const MOVE = "move";
 export const GAME_OVER = "game_over";
 
+interface Metadata {
+    blackPlayer: string;
+    whitePlayer: string;
+}
+
 export const Game = () => {
     const socket = useSocket();
     const { gameId } = useParams();
@@ -21,6 +26,7 @@ export const Game = () => {
     const [chess, _setChess] = useState(new Chess());
     const [board, setBoard] = useState(chess.board());
     const [started, setStarted] = useState(false)
+    const [gameMetadata, setGameMetadata] = useState<Metadata | null>(null)
 
     useEffect(() => {
         if (!socket) {
@@ -34,6 +40,10 @@ export const Game = () => {
                     setBoard(chess.board());
                     setStarted(true)
                     navigate(`/game/${message.payload.gameId}`)
+                    setGameMetadata({
+                        blackPlayer: message.payload.blackPlayer,
+                        whitePlayer: message.payload.whitePlayer
+                    })
                     break;
                 case MOVE:
                     const move = message.payload;
@@ -49,21 +59,26 @@ export const Game = () => {
 
     if (!socket) return <div>Connecting...</div>
 
-    return <div className="justify-center flex">
-        <div className="pt-8 max-w-screen-lg w-full">
-            <div className="grid grid-cols-6 gap-4 w-full">
-                <div className="col-span-4 w-full flex justify-center">
-                    <ChessBoard chess={chess} setBoard={setBoard} socket={socket} board={board} />
-                </div>
-                <div className="col-span-2 bg-slate-900 w-full flex justify-center">
-                    <div className="pt-8">
-                        {!started && gameId === "random" && <Button onClick={() => {
-                            socket.send(JSON.stringify({
-                                type: INIT_GAME
-                            }))
-                        }} >
-                            Play
-                        </Button>}
+    return <div className="">
+        <div className="justify-center flex pt-4 text-white">
+            {gameMetadata?.blackPlayer} vs {gameMetadata?.whitePlayer}
+        </div>
+        <div className="justify-center flex">
+            <div className="pt-8 max-w-screen-lg w-full">
+                <div className="grid grid-cols-6 gap-4 w-full">
+                    <div className="col-span-4 w-full flex justify-center">
+                        <ChessBoard chess={chess} setBoard={setBoard} socket={socket} board={board} />
+                    </div>
+                    <div className="col-span-2 bg-slate-900 w-full flex justify-center">
+                        <div className="pt-8">
+                            {!started && gameId === "random" && <Button onClick={() => {
+                                socket.send(JSON.stringify({
+                                    type: INIT_GAME
+                                }))
+                            }} >
+                                Play
+                            </Button>}
+                        </div>
                     </div>
                 </div>
             </div>
