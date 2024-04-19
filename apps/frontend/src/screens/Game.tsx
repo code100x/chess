21509412cf706de0325793839218d53f04990a81
Rahil Vahora <104057403+rahil1202@users.noreply.github.com>
2024-04-19 20:43:28 +0530

    Added Confetti

--- apps/frontend/src/screens/Game.tsx
+++ apps/frontend/src/screens/Game.tsx
@@ -6,6 +6,7 @@ import { ChessBoard } from "../components/ChessBoard"
 import { useSocket } from "../hooks/useSocket";
 import { Chess } from 'chess.js'
 import { useNavigate, useParams } from "react-router-dom";
+import ReactConfetti from 'react-confetti';
 
 // TODO: Move together, there's code repetition here
 export const INIT_GAME = "init_game";
@@ -13,79 +14,80 @@ export const MOVE = "move";
 export const GAME_OVER = "game_over";
 
 interface Metadata {
-    blackPlayer: string;
-    whitePlayer: string;
+  blackPlayer: string;
+  whitePlayer: string;
 }
 
 export const Game = () => {
-    const socket = useSocket();
-    const { gameId } = useParams();
+  const socket = useSocket();
+  const { gameId } = useParams();
+  const navigate = useNavigate();
+      // Todo move to store/context
 
-    const navigate = useNavigate();
-    // Todo move to store/context
-    const [chess, _setChess] = useState(new Chess());
-    const [board, setBoard] = useState(chess.board());
-    const [started, setStarted] = useState(false)
-    const [gameMetadata, setGameMetadata] = useState<Metadata | null>(null)
-    const [result, setResult] = useState<"WHITE_WINS" | "BLACK_WINS" | "DRAW">(false);
+  const [chess, _setChess] = useState(new Chess());
+  const [board, setBoard] = useState(chess.board());
+  const [started, setStarted] = useState(false)
+  const [gameMetadata, setGameMetadata] = useState<Metadata | null>(null)
+  const [result, setResult] = useState<"WHITE_WINS" | "BLACK_WINS" | "DRAW">(false);
+  const [showConfetti, setShowConfetti] = useState(false);
 
-    useEffect(() => {
-        if (!socket) {
-            return;
-        }
-        socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      switch (message.type) {
+        case INIT_GAME:
+          setBoard(chess.board());
+          setStarted(true)
+          navigate(`/game/${message.payload.gameId}`)
+          setGameMetadata({ blackPlayer: message.payload.blackPlayer, whitePlayer: message.payload.whitePlayer })
+          break;
+        case MOVE:
+          const move = message.payload;
+          chess.move(move);
+          setBoard(chess.board());
+          break;
+        case GAME_OVER:
+          setResult(message.payload.result);
+          setShowConfetti(true);
+          break;
+      }
+    }
+  }, [chess, socket]);
 
-            switch (message.type) {
-                case INIT_GAME:
-                    setBoard(chess.board());
-                    setStarted(true)
-                    navigate(`/game/${message.payload.gameId}`)
-                    setGameMetadata({
-                        blackPlayer: message.payload.blackPlayer,
-                        whitePlayer: message.payload.whitePlayer
-                    })
-                    break;
-                case MOVE:
-                    const move = message.payload;
-                    chess.move(move);
-                    setBoard(chess.board());
-                    break;
-                case GAME_OVER:
-                    setResult(message.payload.result);
-                    break;
-            }
-        }
-    }, [chess, socket]);
+  if (!socket) return <div>Connecting...</div>
 
-    if (!socket) return <div>Connecting...</div>
-
-    return <div className="">
-        <div className="justify-center flex pt-4 text-white">
-            {gameMetadata?.blackPlayer} vs {gameMetadata?.whitePlayer}
-        </div>
-        {result && <div className="justify-center flex pt-4 text-white">
-            {result}    
-        </div>}
-        <div className="justify-center flex">
-            <div className="pt-8 max-w-screen-lg w-full">
-                <div className="grid grid-cols-6 gap-4 w-full">
-                    <div className="col-span-4 w-full flex justify-center">
-                        <ChessBoard chess={chess} setBoard={setBoard} socket={socket} board={board} />
-                    </div>
-                    <div className="col-span-2 bg-slate-900 w-full flex justify-center">
-                        <div className="pt-8">
-                            {!started && gameId === "random" && <Button onClick={() => {
-                                socket.send(JSON.stringify({
-                                    type: INIT_GAME
-                                }))
-                            }} >
-                                Play
-                            </Button>}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="">
+      {showConfetti && <ReactConfetti />}
+      <div className="justify-center flex pt-4 text-white">
+        {gameMetadata?.blackPlayer} vs {gameMetadata?.whitePlayer}
+      </div>
+      {result && <div className="justify-center flex pt-4 text-white">
+        {result}
+      </div>}
+      <div className="justify-center flex">
+        <div className="pt-8 max-w-screen-lg w-full">
+          <div className="grid grid-cols-6 gap-4 w-full">
+            <div className="col-span-4 w-full flex justify-center">
+              <ChessBoard chess={chess} setBoard={setBoard} socket={socket} board={board} />
             </div>
+            <div className="col-span-2 bg-slate-900 w-full flex justify-center">
+              <div className="pt-8">
+                {!started && gameId === "random" &&
+                  <Button onClick={() => {
+                    socket.send(JSON.stringify({ type: INIT_GAME }))
+                  }}>
+                    Play
+                  </Button>
+                }
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-}
+  );
+};
