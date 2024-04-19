33a610710b3744ab8d57ef19516d4c52141ee4fc
vishal mishra <vishal.mishra9470@gmail.com>
2024-04-19 18:42:35 +0530

    make sound when move and dragble , as previous merge is removed

--- apps/frontend/src/components/ChessBoard.tsx
+++ apps/frontend/src/components/ChessBoard.tsx
@@ -2,62 +2,107 @@ import { Chess, Color, PieceSymbol, Square } from "chess.js";
 import { useState } from "react";
 import { MOVE } from "../screens/Game";
 
-export const ChessBoard = ({ chess, board, socket, setBoard }: {
-    chess: Chess;
-    setBoard: React.Dispatch<React.SetStateAction<({
+export const ChessBoard = ({ chess, board, socket, setBoard,}: {
+  chess: Chess;
+    setBoard: React.Dispatch<
+    React.SetStateAction<
+      ({
         square: Square;
         type: PieceSymbol;
         color: Color;
-    } | null)[][]>>;
-    board: ({
-        square: Square;
-        type: PieceSymbol;
-        color: Color;
-    } | null)[][];
-    socket: WebSocket;
+      } | null)[][]
+    >
+  >;
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+  socket: WebSocket;
 }) => {
-    const [from, setFrom] = useState<null | Square>(null);
+  const [from, setFrom] = useState<null | Square>(null);
 
-    return <div className="text-white-200">
-        {board.map((row, i) => {
-            return <div key={i} className="flex">
-                {row.map((square, j) => {
-                    const squareRepresentation = String.fromCharCode(97 + (j % 8)) + "" + (8 - i) as Square;
+  const handleDragStart = (e: React.DragEvent, square: Square) => {
+    e.dataTransfer.setData("text/plain", square);
+    setFrom(square);
+  };
+  const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 
-                    return <div onClick={() => {
-                        if (!from) {
-                            setFrom(squareRepresentation);
-                        } else {
-                            socket.send(JSON.stringify({
-                                type: MOVE,
-                                payload: {
-                                    move: {
-                                        from,
-                                        to: squareRepresentation
-                                    }
-                                }
-                            }))
-                            
-                            setFrom(null)
-                            chess.move({
-                                from,
-                                to: squareRepresentation
-                            });
-                            setBoard(chess.board());
-                            console.log({
-                                from,
-                                to: squareRepresentation
-                            })
-                        }
-                    }} key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-green-500' : 'bg-slate-500'}`}>
-                        <div className="w-full justify-center flex h-full">
-                            <div className="h-full justify-center flex flex-col">
-                                {square ? <img className="w-4" src={`/${square?.color === "b" ? square?.type : `${square?.type?.toUpperCase()} copy`}.png`} /> : null} 
-                            </div>
-                        </div>
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+    audio.play();
+  };
+
+  const handleDrop = (e: React.DragEvent, to: Square) => {
+    e.preventDefault();
+    if (from) {
+      socket.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: {
+            move: {
+              from,
+              to,
+            },
+          },
+        })
+      );
+
+      chess.move({
+        from,
+        to,
+      });
+      setBoard(chess.board());
+      setFrom(null);
+    }
+  };
+
+
+  return (
+    <div className="text-white-200">
+      {board.map((row, i) => {
+        return (
+          <div key={i} className="flex">
+            {row.map((square, j) => {
+              const squareRepresentation = (String.fromCharCode(97 + (j % 8)) +
+                "" +
+                (8 - i)) as Square;
+
+              return (
+                <div
+                  key={j}
+                  className={`w-16 h-16 ${
+                    (i + j) % 2 === 0 ? "bg-green-500" : "bg-slate-500"
+                  }`}
+                  onDragOver={handleDragOver}
+                  onDrop={(e) => handleDrop(e, squareRepresentation)}
+                >
+                  <div className="w-full justify-center flex h-full">
+                    <div className="h-full justify-center flex flex-col">
+                      {square ? (
+                        <img
+                          className="w-4"
+                          src={`/${
+                            square?.color === "b"
+                              ? square?.type
+                              : `${square?.type?.toUpperCase()} copy`
+                          }.png`}
+                          draggable={true}
+                          onDragStart={(e) =>
+                            handleDragStart(e, squareRepresentation)
+                          }
+                        
+                          
+                        />
+                      ) : null}
                     </div>
-                })} 
-            </div>
-        })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
-}
+  );
+};
