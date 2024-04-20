7208b1a12ea82de7e4c17d3a54abfd75038fbfee
Javed <javedofficialme@gmail.com>
2024-04-20 06:17:31 +0530

    fix: increase session time

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -14,7 +14,7 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, maxAge: 360000}
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000}
 }));
 
 initPassport();
