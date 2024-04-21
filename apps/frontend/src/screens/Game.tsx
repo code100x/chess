7590737fb1cee02370dc370e8b0445c3b13dd095
vishal mishra <vishal.mishra9470@gmail.com>
2024-04-21 13:10:54 +0530

    move table ui fix

--- apps/frontend/src/screens/Game.tsx
+++ apps/frontend/src/screens/Game.tsx
@@ -171,7 +171,7 @@ export const Game = () => {
                 </div>
               </div>
             </div>
-            <div className="col-span-2 bg-brown-500 w-full flex justify-center h-[90vh] overflow-scroll mt-10">
+            <div className="col-span-2 bg-brown-500  w-full flex justify-center h-[90vh] overflow-scroll mt-8">
               {!started && (
                 <div className="pt-8">
                   {added ? (
