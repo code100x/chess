a80224dce250a78220d29d0ef0dcc3853b8d311e
sourabh@32 <sourabhsharma181003@gmail.com>
2024-04-23 16:12:02 +0530

    impoved ( Loader UI)

--- apps/frontend/src/components/Loader.tsx
+++ apps/frontend/src/components/Loader.tsx
@@ -1,4 +1,9 @@
 //TODO: Make prettier
 export const Loader = () => {
-  return <div>Loading..</div>;
+  return <div className="w-full min-h-[100vh] flex items-center justify-center">
+    <div className="flex-col gap-2">
+      <img src={"/public/loader.png"} className={"w-20 h-20"} />
+      <p className="text-white text-xl text-bold">Loading...</p>
+    </div>
+  </div>;
 };
