81c3b6b5f5375a8c8310c2be6e08288b4aa9a879
javed <javed@local.com>
2024-04-29 18:27:48 +0530

    chore: animate splash screen

--- apps/chess-mobile/src/app/_layout.tsx
+++ apps/chess-mobile/src/app/_layout.tsx
@@ -2,8 +2,24 @@ import { AuthProvider } from '~/context/authcontext';
 import '../global.css';
 
 import { Slot } from 'expo-router';
+import { useState } from 'react';
+import SplashScreen from '~/components/SplashScreen';
 
 export default function Layout() {
+  const [splashAnimationFinished, setSplashAnimationFinished] = useState(false);
+  if (!splashAnimationFinished) {
+    return (
+      <SplashScreen
+        onAnimationFinish={(isCancelled) => {
+          console.log("isCancelled", isCancelled);
+          if (!isCancelled) {
+            setSplashAnimationFinished(true);
+          }
+        }}
+      />
+    )
+  }
+
   return (
     <AuthProvider>
       <Slot />
