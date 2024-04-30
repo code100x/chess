5790c6efb06e91230861f5675a81ad417b3a649a
gaurav12devloper <gauravprajapati459@gmail.com>
2024-04-30 20:47:40 +0530

    Added user avatar

--- apps/frontend/src/components/UserAvatar.tsx
+++ apps/frontend/src/components/UserAvatar.tsx
@@ -1,3 +1,10 @@
-export const UserAvatar = ({ name }: { name: string }) => {
-  return <div className="text-white">{name}</div>;
+export const UserAvatar = ({ name , avatar }: { name: string , avatar:string}) => {
+  return (
+    <div className="flex  gap-4">
+    <img className="w-10 h-10 rounded" src={avatar} alt="" />
+    <div className="font-medium dark:text-white text-white">
+      <div>{name}</div>
+    </div>
+</div>
+  )
 };
