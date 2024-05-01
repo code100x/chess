6d41ca5a8903e1aa72752c837f4c0b9b0496156e
adi790uu <adi37068@gmail.com>
2024-05-01 16:36:55 +0530

    improved loader screen

--- apps/frontend/src/components/Loader.tsx
+++ apps/frontend/src/components/Loader.tsx
@@ -1,4 +1,40 @@
-//TODO: Make prettier
-export const Loader = () => {
-  return <div>Loading..</div>;
+import { motion } from 'framer-motion';
+
+const Loader = () => {
+  return (
+    <div className="fixed chess-board top-0 left-0 w-full h-screen flex justify-center items-center z-10">
+      <div className="p-4 rounded-md">
+        <div className="flex justify-center">
+          <>
+            <motion.span
+              className="w-2 h-2 my-12 mx-1 bg-white rounded-full"
+              animate={{
+                y: [0, -20, 0],
+                opacity: [1, 0],
+                transition: { duration: 1, repeat: Infinity },
+              }}
+            />
+            <motion.span
+              className="w-2 h-2 my-12 mx-1 bg-white rounded-full"
+              animate={{
+                y: [0, -20, 0],
+                opacity: [1, 0],
+                transition: { duration: 1, repeat: Infinity, delay: 0.2 },
+              }}
+            />
+            <motion.span
+              className="w-2 h-2 my-12 mx-1 bg-white rounded-full"
+              animate={{
+                y: [0, -20, 0],
+                opacity: [1, 0],
+                transition: { duration: 1, repeat: Infinity, delay: 0.4 },
+              }}
+            />
+          </>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Loader;
