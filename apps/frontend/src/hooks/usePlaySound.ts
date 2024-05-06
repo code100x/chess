978f23edd308b454198d12450b62bfcd6b0a6ad6
Nimit <nimitharia007@gmail.com>
2024-05-06 11:11:10 +0530

    ts config changes

--- apps/frontend/src/hooks/usePlaySound.ts
+++ apps/frontend/src/hooks/usePlaySound.ts
@@ -1,8 +1,10 @@
 import { Move } from 'chess.js';
+import MoveSound from '/move.wav';
+import CaptureSound from '/capture.wav';
 
 export const usePlaySound = () => {
-  const moveAudio = new Audio('/move.wav');
-  const captureAudio = new Audio('/capture.wav');
+  const moveAudio = new Audio(MoveSound);
+  const captureAudio = new Audio(CaptureSound);
 
   const playSound = (move: Move) => {
     if (move.captured) {
