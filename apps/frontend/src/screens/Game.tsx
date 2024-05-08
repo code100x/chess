914c04cf7ad6c9ca29bedeb5c3f4533fd97b2358
mvking5464 <mvking5464@gmail.com>
2024-05-08 11:30:57 +0530

    stop timer and show result instead of turn after game ends

--- apps/frontend/src/screens/Game.tsx
+++ apps/frontend/src/screens/Game.tsx
@@ -33,7 +33,6 @@ export interface GameResult {
   by: string;
 }
 
-
 const GAME_TIME_MS = 10 * 60 * 1000;
 
 import { useRecoilValue, useSetRecoilState } from 'recoil';
@@ -60,10 +59,7 @@ export const Game = () => {
   const [added, setAdded] = useState(false);
   const [started, setStarted] = useState(false);
   const [gameMetadata, setGameMetadata] = useState<Metadata | null>(null);
-  const [result, setResult] = useState<
-    GameResult
-    | null
-  >(null);
+  const [result, setResult] = useState<GameResult | null>(null);
   const [player1TimeConsumed, setPlayer1TimeConsumed] = useState(0);
   const [player2TimeConsumed, setPlayer2TimeConsumed] = useState(0);
 
@@ -130,8 +126,12 @@ export const Game = () => {
           break;
 
         case GAME_ENDED:
-          const wonBy = message.payload.status === 'COMPLETED' ? 
-            message.payload.result !== 'DRAW' ? 'CheckMate' : 'Draw' : 'Timeout';
+          const wonBy =
+            message.payload.status === 'COMPLETED'
+              ? message.payload.result !== 'DRAW'
+                ? 'CheckMate'
+                : 'Draw'
+              : 'Timeout';
           setResult({
             result: message.payload.result,
             by: wonBy,
@@ -147,8 +147,7 @@ export const Game = () => {
             blackPlayer: message.payload.blackPlayer,
             whitePlayer: message.payload.whitePlayer,
           });
-          
-        
+
           break;
 
         case USER_TIMEOUT:
@@ -199,7 +198,7 @@ export const Game = () => {
   }, [chess, socket]);
 
   useEffect(() => {
-    if (started) {
+    if (started && !result) {
       const interval = setInterval(() => {
         if (chess.turn() === 'w') {
           setPlayer1TimeConsumed((p) => p + 100);
@@ -240,8 +239,16 @@ export const Game = () => {
         <div className="justify-center flex pt-4 text-white">
           {(user.id === gameMetadata?.blackPlayer?.id ? 'b' : 'w') ===
           chess.turn()
-            ? 'Your turn'
-            : "Opponent's turn"}
+            ? result
+              ? result?.result == 'BLACK_WINS'
+                ? 'You won'
+                : 'Opponent won'
+              : 'Your turn'
+            : result
+              ? result?.result == 'WHITE_WINS'
+                ? 'You won'
+                : 'Opponent won'
+              : "Opponent's turn"}
         </div>
       )}
       <div className="justify-center flex">
@@ -269,9 +276,7 @@ export const Game = () => {
                     )}
                   </div>
                   <div>
-                    <div
-                      className={`w-full flex justify-center text-white`}
-                    >
+                    <div className={`w-full flex justify-center text-white`}>
                       <ChessBoard
                         started={started}
                         gameId={gameId ?? ''}
