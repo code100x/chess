6326abed6d5d9e746201042809700ddddf30ea22
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-05-12 23:03:32 +0530

    Changed the loader and make it looks good.

--- apps/frontend/src/components/Loader.tsx
+++ apps/frontend/src/components/Loader.tsx
@@ -1,4 +1,7 @@
 //TODO: Make prettier
+import './styles/Loader.css';
 export const Loader = () => {
-  return <div>Loading..</div>;
+  return (
+    <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-24 w-24"></div>
+  );
 };
