90daa19c6cf3a5f3d03bdb7d7b6b87c1555c2e26
abhay-raj19 <rajpootabhay423@gmail.com>
2024-05-25 06:44:04 +0530

    Feat:removed up the loading screen
    Signed-off-by: abhay-raj19 <rajpootabhay423@gmail.com>

--- apps/frontend/src/components/Loader.tsx
+++ apps/frontend/src/components/Loader.tsx
@@ -1,4 +1,18 @@
 //TODO: Make prettier
+import { BallTriangle } from 'react-loader-spinner'
+
 export const Loader = () => {
-  return <div>Loading..</div>;
+  return <div className='h-screen flex items-center justify-center'>
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            />
+          </div>
+
 };
