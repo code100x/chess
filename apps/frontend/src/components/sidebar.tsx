fe9ca43433b92543331fede506a6e723b67c30ee
somnath <somnathmishra100dbi@gmail.com>
2024-07-02 14:37:46 +0530

    fixed sidebar UI

--- apps/frontend/src/components/sidebar.tsx
+++ apps/frontend/src/components/sidebar.tsx
@@ -36,26 +36,22 @@ export default function Sidebar({ className }: SidebarProps) {
     <nav
       className={cn(
         `relative hidden h-screen pt-4 md:block bg-bgAuxiliary1 text-white w-12 lg:w-36 top-0 relative sticky`,
-        className,
+        className
       )}
     >
       <div className="flex flex-col h-full justify-between">
         <div className="flex flex-col justify-start">
-          {isOpen && (
-            <span className="text-center text-2xl font-bold tracking-tighter ">
-              100xchess
-            </span>
-          )}
+          {isOpen && <span className="text-center text-2xl font-bold tracking-tighter ">100xchess</span>}
 
           <SideNav
-            className="opacity-0 transition-all duration-300 group-hover:z-50  group-hover:rounded group-hover:bg-foreground group-hover:opacity-100"
+            className="opacity-0 transition-all duration-300 group-hover:z-50  group-hover:rounded group-hover:bg-black p-1 group-hover:opacity-100"
             items={UpperNavItems}
           />
         </div>
 
         <div className="flex flex-col justify-end mb-2">
           <SideNav
-            className="opacity-0 transition-all duration-300 group-hover:z-50  group-hover:rounded group-hover:bg-foreground group-hover:opacity-100"
+            className="opacity-0 transition-all duration-300 group-hover:z-50  group-hover:rounded group-hover:bg-black p-1 group-hover:opacity-100"
             items={LowerNavItems}
           />
         </div>
