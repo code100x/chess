e150b66b9b9c21584e9a7f8127914f14f6ba08b0
ajey35 <ajeyakumara07@gmail.com>
2025-02-04 18:35:35 +0530

    Make input box text visible on login page

--- apps/frontend/src/screens/Login.tsx
+++ apps/frontend/src/screens/Login.tsx
@@ -3,8 +3,7 @@ import { useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { userAtom } from '@repo/store/userAtom';
 
-const BACKEND_URL =
-  import.meta.env.VITE_APP_BACKEND_URL ?? 'http://localhost:3000';
+const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL ?? 'http://localhost:3000';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -37,9 +36,7 @@ const Login = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-textMain">
-      <h1 className="text-4xl font-bold mb-8 text-center text-green-500 drop-shadow-lg">
-        Enter the Game World
-      </h1>
+      <h1 className="text-4xl font-bold mb-8 text-center text-green-500 drop-shadow-lg">Enter the Game World</h1>
       <div className="bg-bgAuxiliary2 rounded-lg shadow-lg p-8 flex flex-col md:flex-row">
         <div className="mb-8 md:mb-0 md:mr-8 justify-center flex flex-col">
           <div
@@ -67,7 +64,7 @@ const Login = () => {
             type="text"
             ref={guestName}
             placeholder="Username"
-            className="border px-4 py-2 rounded-md mb-4 w-full md:w-64"
+            className="border px-4 py-2 rounded-md mb-4 w-full md:w-64 text-black"
           />
           <button
             className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors duration-300"
---
+++ packages/db/prisma/migrations/20250201045322_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "AuthProvider" ADD VALUE 'GUEST';
